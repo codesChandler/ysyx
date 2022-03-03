e9c88ad5552366cbfe68e2ea6646cfa21ead18e2
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-03 14:15:00 +0800

    >  sim RTL ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:15:00 up 54 min,  3 users,  load average: 0.00, 0.02, 0.11

---
+++ npc/top_tb.cpp
@@ -0,0 +1,49 @@
+ #include <verilated.h> //核心头文件
+ #include <verilated_vcd_c.h> //波形生成头文件
+#include <iostream>
+#include <fstream>
+ #include "Vtop.h"  //模块类
+
+ using namespace std;
+ Vtop* top; //顶层dut对象指针
+ VerilatedVcdC* tfp; //波形生成对象指针
+
+ vluint64_t main_time = 0;           // 仿真时间戳
+const vluint64_t sim_time = 1024;   // 最大仿真时间戳
+
+int main(int argc, char **argv)
+{
+    // 一些初始化工作
+    Verilated::commandArgs(argc, argv);
+    Verilated::traceEverOn(true);
+
+	// 为对象分配内存空间
+    top = new VDecoder;
+    tfp = new VerilatedVcdC;
+
+    // tfp初始化工作
+    top->trace(tfp, 99);
+    tfp->open("Decoder.vcd");
+
+    int count = 0;
+
+    while(!Verilated::gotFinish() && main_time < sim_time)
+    {
+        // 仿真过程
+		int a = rand() & 1;
+		int b = rand() & 1;
+		top->a = a;
+		top->b = b;
+        top->eval();            // 仿真时间步进
+        tfp->dump(main_time);   // 波形文件写入步进
+        count++;
+        main_time++;
+    }
+
+    // 清理工作
+    tfp->close();
+    delete top;
+    delete tfp;
+    exit(0);
+    return 0;
+}
