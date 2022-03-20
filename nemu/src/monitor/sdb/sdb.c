9989be055eea49f33368a50404188a31f9a0fda5
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 16:42:54 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:42:54 up 46 min,  1 user,  load average: 0.25, 0.20, 0.21

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -39,7 +39,7 @@ static int cmd_q(char *args) {
 
 static int cmd_si(char *args)  {
   char *arg = strtok(strtok(args, " ")," ");
-  printf("the number of instruction:%d /n",*arg);
+  printf("the number of instruction:%d \n",*arg);
   cpu_exec(*arg);
   return 0;
 }
