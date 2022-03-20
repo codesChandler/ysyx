6b8d901b22b149b82720a37a26981383bd0f85a7
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 22:23:31 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:23:31 up  6:27,  1 user,  load average: 0.40, 0.33, 0.32

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -13,7 +13,10 @@ uint8_t *guest_to_host(paddr_t paddr)
 {
   return pmem + paddr - CONFIG_MBASE;
 }
-paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
+paddr_t host_to_guest(uint8_t *haddr) 
+{ 
+  return haddr - pmem + CONFIG_MBASE; 
+}
 
 static word_t pmem_read(paddr_t addr, int len)
 {
