e086a107b606dd25346c3fc8a9451bf4b34edfa2
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 20:41:06 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:41:06 up  4:44,  1 user,  load average: 0.30, 0.22, 0.11

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -10,7 +10,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for(int i=0;i<32;i++)
-  printf("%s",regs[i]);
+  printf("%s\n",regs[i]);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
