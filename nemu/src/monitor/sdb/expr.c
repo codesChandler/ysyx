4fae0c8cf649f6b01d1e3932a8aed88dae59e8ea
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 19:49:33 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:49:33 up 2 days,  6:15,  1 user,  load average: 0.46, 0.39, 0.34

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,4 +1,5 @@
 #include <isa.h>
+#include <common.h>
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
@@ -6,11 +7,17 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
+  TK_NOTYPE = 256, TK_EQ,TK_NR,
 
   /* TODO: Add more token types */
 
 };
+
+typedef struct Point{
+      int x;
+      int y;
+} point;//mark the number
+
 
 static struct rule {
   const char *regex;
@@ -20,10 +27,15 @@ static struct rule {
   /* TODO: Add more rules.
    * Pay attention to the precedence level of different rules.
    */
-
+  {"\\d+", TK_NR},        // number
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
+  {"-", '-'},          // minus
+  {"\\*",'*'},          //multiplication
+  {"/",'/'},             //division 
   {"==", TK_EQ},        // equal
+  {"\\(", '('},         //left bracket
+  {"\\)",')'},          //right bracket
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -38,7 +50,7 @@ void init_regex() {
   char error_msg[128];
   int ret;
 
-  for (i = 0; i < NR_REGEX; i ++) {
+  for (i = 0; i < NR_REGEX; i++) {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
     if (ret != 0) {
       regerror(ret, &re[i], error_msg, 128);
@@ -80,8 +92,15 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          default: TODO();
+          TK_NR: {
+                  for(int i=0;i<substr_len;i++){
+                  tokens[nr_token].str[i]= *(substr_start+i);}
+                  tokens[nr_token].type=rules[i].token_type;
+                  break;}          
+          default: tokens[nr_token].type=rules[i].token_type;break;
         }
+
+        nr_token++;
 
         break;
       }
@@ -96,6 +115,115 @@ static bool make_token(char *e) {
   return true;
 }
 
+//stack implement brackt matchment
+
+bool check_parentheses(int p,int q){
+  int top=-1;
+  int cnt_l=0;
+  int cnt_r=0;
+
+  for (int i=p;i<=q;i++)
+    {if(tokens[i].type!='(')
+      cnt_l++;
+     else if(tokens[i].type!=')')
+      cnt_r++;
+    }
+  
+  if(tokens[p].type!='(' || tokens[q].type!=')')
+    return false;
+  else if(cnt_l!=cnt_r)
+    return false;
+  
+  for(int i=p+1;i<=q-1;i++)
+  {if(tokens[i].type!='(')
+      top++;
+     else if(tokens[i].type!=')')
+      {top--;
+      if (top <-1)
+      return false;
+      }
+    }
+
+    return true;
+}
+
+word_t eval(int p, int q) {
+  if (p > q) {
+    /* Bad expression */
+    assert(0);
+  }
+  else if (p == q) {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+    return atoi(tokens[p].str);
+  }
+  else if (check_parentheses(p, q) == true) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(p + 1, q - 1);
+  }
+  else {
+
+    //find the positon of bracket
+
+    point points[16];
+    int cnt=0;
+    int len=0;
+
+    for (int i=p;i<=q;i++){
+      
+    {if(tokens[i].type!='(')
+      {points[cnt].x=i;
+      cnt++;
+      len++;}
+     else if(tokens[i].type!=')')
+      {points[--cnt].y=i;}
+    }
+    assert(cnt);
+    }
+
+    //find the position of dominant symbol
+    int ops[32];
+    int len_ops=0;
+    for (int i=p;i<=q;i++){
+      for(int j=0;j<len;j++)
+      {
+        if(i>=points[j].x && i<=points[j].y)
+        break;
+        ops[len_ops++]=i;
+      }
+    }
+    int op=32;
+    for (int i=0;i<len_ops;i++){
+      if(tokens[ops[i]].type=='*'|| tokens[ops[i]].type=='/')
+      {op=ops[i];
+      break;}
+    }
+    
+    for (int i=0;i<len_ops;i++)
+      if ((tokens[ops[i]].type=='+'|| tokens[ops[i]].type=='-') && (op==32))
+      {
+        op=ops[i];
+        break;
+      }
+    
+
+    //op = the position of 主运算符 in the token expression;
+    word_t val1 = eval(p, op - 1);
+    word_t val2 = eval(op + 1, q);
+
+    switch (tokens[op].type) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': return val1 / val2;
+      default: assert(0);
+    }
+  }
+}
 
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
@@ -104,7 +232,8 @@ word_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  //TODO();
+  return eval(0,--nr_token);
 
-  return 0;
+  //return 0;
 }
--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -4,40 +4,41 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+word_t expr(char *, bool *);
 
-// word_t char2dec(char *hex)
-// {
-//   int len;
-//   word_t num = 0;
+word_t char2dec(char *hex)
+{
+  int len;
+  word_t num = 0;
 
-//   len = strlen(hex);
+  len = strlen(hex);
 
-//   for (int i = 0, temp = 0; i < len; i++, temp = 0)
-//   {
-//     temp = (*(hex + i) - 48)*10^(len - i - 1);
-//     num = num + temp;
-//   }
+  for (int i = 0, temp = 0; i < len; i++, temp = 0)
+  {
+    temp = (*(hex + i) - 48)*10^(len - i - 1);
+    num = num + temp;
+  }
 
-//   return num;
-// }
+  return num;
+}
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 
-// FILE * fp;
-// fp =fopen("~/ysyx-workbench/nemu/tools/gen-expr/input","r");
-// bool *success=false;
-// for(int i=0;i<200;i++){
-//   char ref_result[100];
-//   word_t result;
-//   char expre[60300];
-//   fscanf(fp,"%s",ref_result);
-//   word_t ref_result_num=char2dec(ref_result);
-//   fscanf(fp,"%s",expre);
-//   result=expr(expre,success);
-//   if(result != ref_result_num || !success)
-//   printf("I am wrong");
-// }
+FILE * fp;
+fp =fopen("~/ysyx-workbench/nemu/tools/gen-expr/input","r");
+bool *success=false;
+for(int i=0;i<200;i++){
+  char ref_result[100];
+  word_t result;
+  char expre[60300];
+  fscanf(fp,"%s",ref_result);
+  word_t ref_result_num=char2dec(ref_result);
+  fscanf(fp,"%s",expre);
+  result=expr(expre,success);
+  if(result != ref_result_num || !success)
+  printf("I am wrong");
+}
 
 
 #ifdef CONFIG_TARGET_AM
