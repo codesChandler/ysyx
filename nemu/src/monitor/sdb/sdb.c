7c3149d08bd3d0b78607054e7a699a6ad6f6829e
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 19:34:46 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:34:46 up 2 days,  6:00,  1 user,  load average: 0.48, 0.35, 0.29

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -233,7 +233,7 @@ void sdb_mainloop()
 void init_sdb()
 {
   /* Compile the regular expressions. */
-  //init_regex();
+  init_regex();
 
   /* Initialize the watchpoint pool. */
   init_wp_pool();
