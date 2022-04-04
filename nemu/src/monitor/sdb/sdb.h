3b7880177fb77b0ef07659f57d1ad285cdc5dbf2
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 19:12:35 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:12:35 up 2 days,  5:38,  1 user,  load average: 0.31, 0.33, 0.40

--- nemu/src/monitor/sdb/sdb.h
+++ nemu/src/monitor/sdb/sdb.h
@@ -3,6 +3,4 @@
 
 #include <common.h>
 
-word_t expr(char *e, bool *success);
-
 #endif
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -4,9 +4,11 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
+//#include <unistd.h>
 
 // this should be enough
 static char buf[65536] = {};
+static int op=0;//mark the position of buf point
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
@@ -16,21 +18,79 @@ static char *code_format =
 "  return 0; "
 "}";
 
+int choose(int mol){
+  //printf("choose\n");
+  //srand(time(0));
+  //printf("%d", rand());
+  if(op<6000)
+  return (unsigned) (rand() % mol);
+  else return 0;//prevent buf overflow
+}
+
+void gen_num(){
+  // printf("num\n");
+  //srand((unsigned)time(NULL));
+  char num[3];
+  //printf(" before buf here\n");
+  sprintf(num,"%d",(unsigned)rand()%99);
+  //printf(" after buf here\n");
+  int len=strlen(num);
+  for(int i=0;*(num+i)!='\0';i++)
+  {*(buf+op+i)=*(num+i);}
+  op +=len;
+  //printf("buf here\n");
+  //printf("op: %d i:\n",op);
+}
+
+void gen_rand_op(){
+   //printf("op\n");
+  //srand((unsigned)time(NULL));
+  switch((unsigned)(rand()%4)) {
+    case 0: *(buf+op)='+' ;break;
+    case 1: *(buf+op)='-' ;break;
+    case 2: *(buf+op)='*' ;break;
+    default: *(buf+op)='/';break;
+  }
+  op++;
+  //printf("op: %d",op);
+}
+
+void gen(char sym){
+   //printf("gen: %c\n",sym);
+  *(buf+op)=sym;
+  op++;
+   //printf("op: %d",op);
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  //printf("I am here\n");
+  int n=choose(3);
+   switch (n) {
+    case 0: {//printf("case 0\n");
+              gen_num();break;}
+    case 1: {//printf("case 1\n");
+              gen('('); gen_rand_expr(); gen(')'); break;
+              }
+    default: {//printf("case 2\n");
+              gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+            }
+  }
+  *(buf+op)='\0';
+  //printf("buf: %s\n",buf);
 }
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
   srand(seed);
   int loop = 1;
-  if (argc > 1) {
+  if (argc > 1)
     sscanf(argv[1], "%d", &loop);
-  }
+  
   int i;
-  for (i = 0; i < loop; i ++) {
+  for (i = 0; i < loop; i++) {
+    op=0;
     gen_rand_expr();
-
+    //printf("%s\n",buf);
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
@@ -39,7 +99,7 @@ int main(int argc, char *argv[]) {
     fclose(fp);
 
     int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    if (ret != 0) continue;//exception handling
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
