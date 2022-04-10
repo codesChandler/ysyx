3637dfebdda97a68a12d60ef59e89a7ebd7e3031
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-10 19:57:10 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:57:10 up 21 min,  1 user,  load average: 0.16, 0.18, 0.21

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -37,6 +37,7 @@ for(int i=0;i<200;i++){
   word_t result;
   char expre[60300];
   int m=fscanf(fp,"%s",ref_result);
+  printf("fscanf after");
   word_t ref_result_num=char2dec(ref_result);
   int n=fscanf(fp,"%s",expre);
   if(m==n){printf("I am OK");}
