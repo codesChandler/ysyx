cf55fc1d7b3c937f3a3eb93346e123d186801bd1
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-10 19:45:16 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:45:16 up 9 min,  1 user,  load average: 0.35, 0.45, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,23 +1,16 @@
-#include "isa.h"
-#include "common.h"
+#include <isa.h>
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
-#include "regex.h"
+#include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,TK_NR,
+  TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
 
 };
-
-typedef struct Point{
-      int x;
-      int y;
-} point;//mark the number
-
 
 static struct rule {
   const char *regex;
@@ -27,20 +20,15 @@ static struct rule {
   /* TODO: Add more rules.
    * Pay attention to the precedence level of different rules.
    */
-  {"\\w+", TK_NR},        // number
+
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
-  {"-", '-'},          // minus
-  {"\\*",'*'},          //multiplication
-  {"/",'/'},             //division 
   {"==", TK_EQ},        // equal
-  {"\\(", '('},         //left bracket
-  {"\\)",')'},          //right bracket
 };
 
 #define NR_REGEX ARRLEN(rules)
 
-static regex_t re[NR_REGEX] ;
+static regex_t re[NR_REGEX] = {};
 
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
@@ -50,7 +38,7 @@ void init_regex() {
   char error_msg[128];
   int ret;
 
-  for (i = 0; i < NR_REGEX; i++) {
+  for (i = 0; i < NR_REGEX; i ++) {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
     if (ret != 0) {
       regerror(ret, &re[i], error_msg, 128);
@@ -64,8 +52,8 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[1000]  ;
-static int nr_token  = 0;
+static Token tokens[32] __attribute__((used)) = {};
+static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
   int position = 0;
@@ -77,12 +65,11 @@ static bool make_token(char *e) {
   while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
-     // printf("%s\n",e);
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-       Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
@@ -91,22 +78,10 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-        //int type=rules[i].token_type;
-        //printf("I am here %d",type);
-        //int token_type=TK_NR;
-        //printf("I am here %d\n",token_type);
+
         switch (rules[i].token_type) {
-
-          case TK_NR: {
-                  for(int i=0;i<substr_len;i++){
-                  tokens[nr_token].str[i]= *(substr_start+i);}
-                  tokens[nr_token].str[substr_len]='\0';
-                  tokens[nr_token].type=rules[i].token_type;
-                  break;}          
-          default: tokens[nr_token].type=rules[i].token_type;break;
+          default: TODO();
         }
-
-        nr_token++;
 
         break;
       }
@@ -121,142 +96,15 @@ static bool make_token(char *e) {
   return true;
 }
 
-//stack implement brackt matchment
-
-bool check_parentheses(int p,int q){
-  int top=0;
-  int cnt_l=0;
-  int cnt_r=0;
-
-  for (int i=p;i<=q;i++)
-    {if(tokens[i].type=='(')
-      cnt_l++;
-     else if(tokens[i].type==')')
-      cnt_r++;
-    }
-  
-  if(tokens[p].type!='(' || tokens[q].type!=')')
-    return false;
-  else if(cnt_l!=cnt_r)
-    return false;
-  
-  for(int i=p+1;i<=q-1;i++)
-  {if(tokens[i].type=='(')
-      top++;
-     else if(tokens[i].type==')')
-      {top--;
-      if (top <0)
-      return false;
-      }
-    }
-
-    return true;
-}
-
-word_t eval(int p, int q) {
-  if (p > q) {
-    /* Bad expression */
-    //assert(0);
-  }
-  else if (p == q) {
-    /* Single token.
-     * For now this token should be a number.
-     * Return the value of the number.
-     */
-    return atoi(tokens[p].str);
-  }
-  else if (check_parentheses(p, q) == true) {
-    /* The expression is surrounded by a matched pair of parentheses.
-     * If that is the case, just throw away the parentheses.
-     */
-    return eval(p + 1, q - 1);
-  }
-  else {
-
-    //find the positon of bracket
-
-    point points[320];
-    struct {int location[320];
-            int top;} locations;
-    locations.top=-1;        
-    //int cnt[100];
-    //int pointer=0;
-    int len=0;
-
-    for (int i=p;i<=q;i++){
-      
-    {if(tokens[i].type=='(')
-      {points[len].x=i;
-        locations.location[++locations.top]=len++;
-      }
-     else if(tokens[i].type==')')
-      {
-         points[locations.location[locations.top--]].y=i;}
-    }
-    //assert((cnt==0));
-    }
-
-    //find the position of dominant symbol
-    int ops[10000];
-    int len_ops=0;
-    int flag=0;
-    for (int i=p;i<=q;i++){
-      flag=0;
-      for(int j=0;j<len;j++)
-      {
-        if(i>=points[j].x && i<=points[j].y)
-        flag=1;
-      }
-      if(flag==0){
-        ops[len_ops++]=i;
-      }
-    }
-
-    int op=10000;
-
-    for (int i=len_ops-1;i>=0;i--)
-      if (tokens[ops[i]].type=='+'|| tokens[ops[i]].type=='-') 
-      {
-        op=ops[i];
-        break;
-      }
-
-    for (int i=len_ops-1;i>=0;i--){
-      if((tokens[ops[i]].type=='*'|| tokens[ops[i]].type=='/')&& (op==10000))
-      {op=ops[i];
-      break;}
-    }
-    
-
-    
-
-    //op = the position of 主运算符 in the token expression;
-    word_t val1 = eval(p, op - 1);
-    word_t val2 = eval(op + 1, q);
-
-    switch (tokens[op].type) {
-      case '+': return val1 + val2;
-      case '-': return val1 - val2;
-      case '*': return val1 * val2;
-      case '/': return val1 / val2;
-      default: assert(0);
-    }
-  }
-  return 0;
-}
 
 word_t expr(char *e, bool *success) {
-  //init_regex();
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  //TODO();
-  *success = true;
-  //printf("nr_token:%d",nr_token);
-  return eval(0,--nr_token);
+  TODO();
 
-  //return 0;
+  return 0;
 }
