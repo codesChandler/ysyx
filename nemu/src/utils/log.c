3bbb44fce9107fe99d67dd78c80ccaa96acf26de
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-15 13:41:30 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:41:30 up  1:57,  1 user,  load average: 0.26, 0.30, 0.23

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -15,5 +15,5 @@ void init_log(const char *log_file) {
 
 bool log_enable() {
   return MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
-         (g_nr_guest_inst <= CONFIG_TRACE_END), false);
+         (g_nr_guest_inst <= CONFIG_TRACE_END), true);
 }
