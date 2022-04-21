cc187af086fa8d9dcf3ba3901ba734fe41154446
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 21:56:39 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:56:39 up 12:45,  1 user,  load average: 0.12, 0.25, 0.21

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -9,7 +9,7 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, // none
+  TYPE_N, TYPE_J,// none
 };
 
 #define src1R(n) do { *src1 = R(n); } while (0)
@@ -22,6 +22,11 @@ enum {
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
+static word_t immJ(uint32_t i) { uint64_t _1st=BITS(i, 30, 21)<<1;
+                                 uint64_t _2st=BITS(i,20,20)<<11;
+                                 uint64_t _3st=BITS(i,19,12)<<12;
+                                 return (SEXT(BITS(i, 31, 31), 1) << 19) | _1st | _2st | _3st; }
+
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
@@ -33,6 +38,7 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
     case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
+    case TYPE_J: src1I(immJ(i)); destR(rd);break;
   }
 }
 
@@ -47,10 +53,13 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(dest)=s->dnpc;s->dnpc=src1+s->pc;);
+
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = src1 + s->pc);
 
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + src2, 8));
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", ADDI   , I, R(dest) = src1 + src2);
+
   INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + dest, 8, src2));
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
