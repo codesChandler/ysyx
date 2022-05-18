3f6a06aedadd74464000d7109cc49857dd115123
ysyx_22040632-Xiang Hui <2434967096@qq.com>
2022-05-18 21:57:34 +0800

    complete string.c

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -10,7 +10,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for(int i=0;i<32;i++)
-  printf("%s: 0x%08lx\n",regs[i],cpu.gpr[i]);
+  printf("NO:%d %s: 0x%08lx\n",i,regs[i],cpu.gpr[i]);
 
 }
 
