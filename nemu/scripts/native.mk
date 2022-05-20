8c14f7a6d7a6e5f6512194eb14b009a300b3835c
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-20 15:50:58 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:50:58 up 1 day, 45 min,  1 user,  load average: 0.37, 0.66, 0.51

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -10,7 +10,7 @@ $(BINARY): compile_git
 # Some convenient rules
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
-override ARGS ?= --batch
+override ARGS += --batch
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
