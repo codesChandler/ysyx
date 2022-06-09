b75520424e7f7b62b0515235f2e45be3cfe4eca3
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-09 21:11:18 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:11:18 up  2:25,  1 user,  load average: 1.01, 1.01, 0.83

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -2,11 +2,11 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t npc) {
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0;i<32;i++){
     if(ref_r->gpr[i]!=cpu.gpr[i])
       return false;}
-  if(ref_r->pc!=npc)
+  if(ref_r->pc!=cpu.pc)
     return false;
   return true;
 }
