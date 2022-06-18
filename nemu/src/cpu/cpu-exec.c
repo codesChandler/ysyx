a262ff87327f484b3cdca9942e370a13d061d1d6
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-18 21:57:13 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:57:13 up  5:38,  1 user,  load average: 0.59, 0.52, 0.48

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -99,8 +99,7 @@ int ftrace_imple(Decode *s){
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
-  
-  if (ITRACE_COND) { log_write("%s\n", _this->logbuf);printf("***********/n"); }
+  if (ITRACE_COND) { log_write("%s\n", _this->logbuf);printf("size:%ld ***********/n",sizeof(ITRACE_COND)); }
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
