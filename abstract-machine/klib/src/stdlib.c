d1cd47fbba974397ff3fd5c524102d874b81ca39
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-06 20:51:24 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:51:24 up  9:50,  1 user,  load average: 0.90, 0.38, 0.28

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -28,28 +28,29 @@ int atoi(const char* nptr) {
   }
   return x;
 }
-extern Area heap;
+// extern Area heap;
 
-void *malloc_ptr=NULL;
+// void *malloc_ptr=heap.start;
 
-void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  size  = (size_t)ROUNDUP(size, 8);
-  char *old = malloc_ptr;
-  malloc_ptr += size;
-  assert(((uintptr_t)heap.start <= (uintptr_t)malloc_ptr) & ((uintptr_t)malloc_ptr< (uintptr_t)heap.end));
-  for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)malloc_ptr; p ++) {
-    *p = 0;
-  }
-  // assert((uintptr_t)hbrk - (uintptr_t)heap.start <= setting->mlim);
-  return old;
-  // panic("Not implemented");
-#endif
-  return NULL;
-}
+// void *malloc(size_t size) {
+//   // On native, malloc() will be called during initializaion of C runtime.
+//   // Therefore do not call panic() here, else it will yield a dead recursion:
+//   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+//   if(size == 0) return NULL;
+//   size  = (size_t)ROUNDUP(size, 8);
+//   char *old = malloc_ptr;
+//   malloc_ptr += size;
+//   assert(((uintptr_t)heap.start <= (uintptr_t)malloc_ptr) & ((uintptr_t)malloc_ptr< (uintptr_t)heap.end));
+//   for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)malloc_ptr; p ++) {
+//     *p = 0;
+//   }
+//   // assert((uintptr_t)hbrk - (uintptr_t)heap.start <= setting->mlim);
+//   return old;
+//   // panic("Not implemented");
+// #endif
+//   return NULL;
+// }
 
 void free(void *ptr) {
 }
