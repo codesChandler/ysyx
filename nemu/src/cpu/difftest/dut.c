921423c95428b269ffdb0b26f811ebcd4222a626
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-06 11:34:48 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:34:48 up  1:52,  1 user,  load average: 0.30, 0.24, 0.26

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -15,7 +15,7 @@ void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
 static bool is_skip_ref = false;
 static int skip_dut_nr_inst = 0;
-
+extern void inst_display();
 // this is used to let ref skip instructions which
 // can not produce consistent behavior with NEMU
 void difftest_skip_ref() {
@@ -82,6 +82,7 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
     nemu_state.halt_pc = pc;
     printf("*************DiffTest Wrong*************\n");
     isa_reg_display();
+    inst_display();
   }
 }
 
