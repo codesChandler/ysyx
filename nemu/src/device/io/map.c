a84324beed050e6d47144a533048113cc805b866
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-07 10:59:54 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:59:54 up  1:04,  1 user,  load average: 0.65, 0.45, 0.31

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -40,7 +40,7 @@ void init_map() {
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
   #ifdef CONFIG_DTRACE
-    printf("Device name:%s",map->name);
+    printf("Device Name for Read:%s\n",map->name);
   #endif
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
@@ -51,6 +51,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  #ifdef CONFIG_DTRACE
+    printf("Device Name for Write:%s\n",map->name);
+  #endif
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
