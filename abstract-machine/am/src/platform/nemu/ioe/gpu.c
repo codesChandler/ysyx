cfa394bf4667f5d0586b52bd1a1665b151c21d4d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-08 14:47:08 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:47:08 up  7:15,  1 user,  load average: 1.44, 1.04, 0.85

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,13 +5,12 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  // int i;
-  // int w = inw(VGACTL_ADDR+2);  // TODO: get the correct width
-  // int h = inw(VGACTL_ADDR);  // TODO: get the correct height
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i ++) fb[i] = 111;
-  // // memset((void *)0xa1000000, 100, w*h*4);
-  // outl(SYNC_ADDR, 1);
+  int i;
+  int w = inw(VGACTL_ADDR+2);  // TODO: get the correct width
+  int h = inw(VGACTL_ADDR);  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = 111;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {//显示控制器信息
