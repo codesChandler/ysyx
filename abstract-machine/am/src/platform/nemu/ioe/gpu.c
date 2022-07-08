0c15f98500abcebd977529126401772756cc749c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-08 15:28:52 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:28:52 up  7:56,  1 user,  load average: 1.94, 1.18, 0.72

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -30,7 +30,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {//帧缓冲控制器信息frame buff
   int cnt=0;
   for(int x_=ctl->x;x_<=ctl->x+w;x_++)
     for(int y_=ctl->y;y_<=ctl->y+h;y_++){
-      *(fb+w*y_+x_)=*((uint32_t *)ctl->pixels+cnt);cnt++;}
+      *(fb+y_+x_)=*((uint32_t *)ctl->pixels+cnt);cnt++;}
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
