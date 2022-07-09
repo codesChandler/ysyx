3ffc8474f66fbd087a4eeab9317bc59e8b5f2b71
ysyx_22040632-Xiang Hui <2434967096@qq.com>
2022-07-09 20:40:58 +0800

    before starting pa3

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -9,6 +9,7 @@
     _Log(ASNI_FMT("[%s:%d %s] " format, ASNI_FG_BLUE) "\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
+
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
