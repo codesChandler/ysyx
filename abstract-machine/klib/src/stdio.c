d8cf80cca05aebde51bb6589b46839934ec0f3dc
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-09 22:36:48 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:36:48 up 10 min,  1 user,  load average: 0.51, 0.56, 0.35

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -18,7 +18,7 @@ void itoa(unsigned int n, char * buf)
 }
 
 int printf(const char *fmt, ...) {
-  char out[100];
+  char out[8096];
   va_list ap;
   va_start(ap, fmt);
   int n=vsprintf(out,fmt,ap);
@@ -32,7 +32,7 @@ int printf(const char *fmt, ...) {
 int vsprintf(char *out, const char *fmt, va_list ap) {
   char *str = out;
   int d;
-  char *s,buf[100];
+  char *s,buf[8096];
 
   while (*fmt){
     if((*fmt)=='%'){
@@ -65,38 +65,6 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   
   va_end(ap);
 return str-out;//strlen(out);
-  // char *s;
-  // char *str;
-  // for (str = out; *fmt; fmt++) {
-  //   if (*fmt != '%') {
-  //     *str++ = *fmt;
-  //     continue;
-  //   }
-  //   fmt++;
-  //   switch (*fmt) {
-  //     case 'c':
-  //       *str++ = va_arg(ap, int);
-  //       break;
-  //     // case 'd':
-  //     //   // cannot handle %02d etc. maybe fix future.
-  //     //   str = num2str(str, va_arg(ap, ll), 10);
-  //     //   break;
-  //     // case 'p':
-  //     //   str = num2str(str, va_arg(ap, ll), 16);
-  //     //   break;
-  //     case 's':
-  //       s = va_arg(ap, char *);
-  //       while (*s) *str++ = *s++;
-  //       break;
-  //     default:
-  //       if (*fmt != '%') *str++ = '%';
-  //       if (*fmt) *str++ = *fmt;
-  //       else fmt--;
-  //       break;
-  //   }
-  // }
-  // *str = '\0';
-  // return str - out;
 }
 
 
@@ -119,95 +87,3 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 }
 
 #endif
-// #include <am.h>
-// #include <klib.h>
-// #include <klib-macros.h>
-// #include <stdarg.h>
-
-// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
-// typedef long long ll;
-
-// char* num2str(char *str, ll num, ll base) {
-//   char tmp[32];
-//   if (num < 0) {
-//     *str++ = '-';
-//     num = -num;
-//   }
-//   int len = 0;
-//   if (num == 0) tmp[len++] = 0;
-//   else while (num) {
-//     tmp[len++] = num % base;
-//     num = num / base;
-//   }
-//   while (len-- > 0) {
-//     if (tmp[len] < 10) *str++ = tmp[len] + '0';
-//     else *str++ = tmp[len] - 10 + 'A';
-//   }
-//   return str;
-// }
-
-// int printf(const char *fmt, ...) {
-//   int n;
-//   char buf[8192];
-//   va_list args;
-//   va_start(args, fmt);
-//   n = vsprintf(buf, fmt, args);
-//   va_end(args);
-//   putstr(buf);
-//   return n;
-// }
-
-// int sprintf(char *out, const char *fmt, ...) {
-//   int n;
-//   va_list args;
-//   va_start(args, fmt);
-//   n = vsprintf(out, fmt, args);
-//   va_end(args);
-//   return n;
-// }
-
-// int vsprintf(char *out, const char *fmt, va_list ap) {
-//   char *s;
-//   char *str;
-//   for (str = out; *fmt; fmt++) {
-//     if (*fmt != '%') {
-//       *str++ = *fmt;
-//       continue;
-//     }
-//     fmt++;
-//     switch (*fmt) {
-//       case 'c':
-//         *str++ = va_arg(ap, int);
-//         break;
-//       case 'd':
-//         // cannot handle %02d etc. maybe fix future.
-//         str = num2str(str, va_arg(ap, ll), 10);
-//         break;
-//       case 'p':
-//         str = num2str(str, va_arg(ap, ll), 16);
-//         break;
-//       case 's':
-//         s = va_arg(ap, char *);
-//         while (*s) *str++ = *s++;
-//         break;
-//       default:
-//         if (*fmt != '%') *str++ = '%';
-//         if (*fmt) *str++ = *fmt;
-//         else fmt--;
-//         break;
-//     }
-//   }
-//   *str = '\0';
-//   return str - out;
-// }
-
-// int snprintf(char *out, size_t n, const char *fmt, ...) {
-//   panic("Not implemented");
-// }
-
-// int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-//   panic("Not implemented");
-// }
-
-// #endif
