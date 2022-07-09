f808ffff43103f80a92b08e8008c16b2de497b54
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-09 22:31:16 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:31:16 up 4 min,  1 user,  load average: 0.81, 0.52, 0.23

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,72 +1,23 @@
-// #include <am.h>
-// #include <klib.h>
-// #include <klib-macros.h>
-// #include <stdarg.h>
+#include <am.h>
+#include <klib.h>
+#include <klib-macros.h>
+#include <stdarg.h>
 
-// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-// void itoa(unsigned int n, char * buf)
-// { int len,m;
-//   for(len=0,m=n;m/10!=0;len++)
-//     m/=10;
-//   for(int i=0;i<=len;i++){
-//     *(buf+len-i)=n%10+'0';
-//     n=n/10;
-//   }
-//   // printf("len:%d\n",len);
-//   *(buf+len+1)='\0';
-// }
+void itoa(unsigned int n, char * buf)
+{ int len,m;
+  for(len=0,m=n;m/10!=0;len++)
+    m/=10;
+  for(int i=0;i<=len;i++){
+    *(buf+len-i)=n%10+'0';
+    n=n/10;
+  }
+  // printf("len:%d\n",len);
+  *(buf+len+1)='\0';
+}
 
-// // void input_decoder(va_list ap,char *out, const char *fmt){
-// //   char *str = out;
-// //   int d;
-// //   char *s,buf[100];
-
-// //   while (*fmt){
-// //     if((*fmt)=='%'){
-// //     switch (*(++fmt))
-// //     {
-// //     case 's': /* string */
-// //       s = va_arg(ap, char *);
-// //       while(*s) {*str=*s;str++;s++;}
-// //       break;
-// //     case 'c': /* string */
-// //       *str = (char)va_arg(ap, int);
-// //       str ++;
-// //       break;    
-// //     case 'd': /* int */
-// //       d = va_arg(ap, int);
-// //       if (d < 0)
-// //       {
-// //         *str = '-';
-// //         str++;
-// //         d = -d;
-// //       }
-// //       // printf("case d n=[%d]\n", d);
-// //       itoa(d, buf);
-// //       memcpy(str, buf, strlen(buf));
-// //       str += strlen(buf);
-// //       break;}fmt++;}
-// //     else {*str=*fmt;
-// //     str++;fmt++;}}
-// //     *str='\0';
-  
-// //   va_end(ap);
-// // }
-
-// int printf(const char *fmt, ...) {
-//   char out[100];
-//   va_list ap;
-//   va_start(ap, fmt);
-//   int n=vsprintf(out,fmt,ap);
-//   va_end(ap);
-//   for(int i=0;i<strlen(out);i++)
-//     putch(*(out+i));
-//   return n;
-//   // return 0;
-// }
-
-// int vsprintf(char *out, const char *fmt, va_list ap) {
+// void input_decoder(va_list ap,char *out, const char *fmt){
 //   char *str = out;
 //   int d;
 //   char *s,buf[100];
@@ -101,110 +52,67 @@
 //     *str='\0';
   
 //   va_end(ap);
-// return str-out;//strlen(out);
 // }
-
-
-// int sprintf(char *out, const char *fmt, ...)
-// {
-//   va_list ap;
-//   va_start(ap, fmt);
-//   int n=vsprintf(out,fmt,ap);
-//   va_end(ap);
-//   return n;
-//   // panic("Not implemented");
-// }
-
-// int snprintf(char *out, size_t n, const char *fmt, ...) {
-//   panic("Not implemented");
-// }
-
-// int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-//   panic("Not implemented");
-// }
-
-// #endif
-#include <am.h>
-#include <klib.h>
-#include <klib-macros.h>
-#include <stdarg.h>
-
-#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
-typedef long long ll;
-
-char* num2str(char *str, ll num, ll base) {
-  char tmp[32];
-  if (num < 0) {
-    *str++ = '-';
-    num = -num;
-  }
-  int len = 0;
-  if (num == 0) tmp[len++] = 0;
-  else while (num) {
-    tmp[len++] = num % base;
-    num = num / base;
-  }
-  while (len-- > 0) {
-    if (tmp[len] < 10) *str++ = tmp[len] + '0';
-    else *str++ = tmp[len] - 10 + 'A';
-  }
-  return str;
-}
 
 int printf(const char *fmt, ...) {
-  int n;
-  char buf[8192];
-  va_list args;
-  va_start(args, fmt);
-  n = vsprintf(buf, fmt, args);
-  va_end(args);
-  putstr(buf);
+  char out[100];
+  va_list ap;
+  va_start(ap, fmt);
+  int n=vsprintf(out,fmt,ap);
+  va_end(ap);
+  for(int i=0;i<strlen(out);i++)
+    putch(*(out+i));
   return n;
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  int n;
-  va_list args;
-  va_start(args, fmt);
-  n = vsprintf(out, fmt, args);
-  va_end(args);
-  return n;
+  // return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  char *s;
-  char *str;
-  for (str = out; *fmt; fmt++) {
-    if (*fmt != '%') {
-      *str++ = *fmt;
-      continue;
-    }
-    fmt++;
-    switch (*fmt) {
-      case 'c':
-        *str++ = va_arg(ap, int);
-        break;
-      case 'd':
-        // cannot handle %02d etc. maybe fix future.
-        str = num2str(str, va_arg(ap, ll), 10);
-        break;
-      case 'p':
-        str = num2str(str, va_arg(ap, ll), 16);
-        break;
-      case 's':
-        s = va_arg(ap, char *);
-        while (*s) *str++ = *s++;
-        break;
-      default:
-        if (*fmt != '%') *str++ = '%';
-        if (*fmt) *str++ = *fmt;
-        else fmt--;
-        break;
-    }
-  }
-  *str = '\0';
-  return str - out;
+  char *str = out;
+  int d;
+  char *s,buf[100];
+
+  while (*fmt){
+    if((*fmt)=='%'){
+    switch (*(++fmt))
+    {
+    case 's': /* string */
+      s = va_arg(ap, char *);
+      while(*s) {*str=*s;str++;s++;}
+      break;
+    case 'c': /* string */
+      *str = (char)va_arg(ap, int);
+      str ++;
+      break;    
+    case 'd': /* int */
+      d = va_arg(ap, int);
+      if (d < 0)
+      {
+        *str = '-';
+        str++;
+        d = -d;
+      }
+      // printf("case d n=[%d]\n", d);
+      itoa(d, buf);
+      memcpy(str, buf, strlen(buf));
+      str += strlen(buf);
+      break;}fmt++;}
+    else {*str=*fmt;
+    str++;fmt++;}}
+    *str='\0';
+  
+  va_end(ap);
+return str-out;//strlen(out);
+}
+
+
+int sprintf(char *out, const char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  int n=vsprintf(out,fmt,ap);
+  va_end(ap);
+  return n;
+  // panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -216,3 +124,95 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 }
 
 #endif
+// #include <am.h>
+// #include <klib.h>
+// #include <klib-macros.h>
+// #include <stdarg.h>
+
+// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+// typedef long long ll;
+
+// char* num2str(char *str, ll num, ll base) {
+//   char tmp[32];
+//   if (num < 0) {
+//     *str++ = '-';
+//     num = -num;
+//   }
+//   int len = 0;
+//   if (num == 0) tmp[len++] = 0;
+//   else while (num) {
+//     tmp[len++] = num % base;
+//     num = num / base;
+//   }
+//   while (len-- > 0) {
+//     if (tmp[len] < 10) *str++ = tmp[len] + '0';
+//     else *str++ = tmp[len] - 10 + 'A';
+//   }
+//   return str;
+// }
+
+// int printf(const char *fmt, ...) {
+//   int n;
+//   char buf[8192];
+//   va_list args;
+//   va_start(args, fmt);
+//   n = vsprintf(buf, fmt, args);
+//   va_end(args);
+//   putstr(buf);
+//   return n;
+// }
+
+// int sprintf(char *out, const char *fmt, ...) {
+//   int n;
+//   va_list args;
+//   va_start(args, fmt);
+//   n = vsprintf(out, fmt, args);
+//   va_end(args);
+//   return n;
+// }
+
+// int vsprintf(char *out, const char *fmt, va_list ap) {
+//   char *s;
+//   char *str;
+//   for (str = out; *fmt; fmt++) {
+//     if (*fmt != '%') {
+//       *str++ = *fmt;
+//       continue;
+//     }
+//     fmt++;
+//     switch (*fmt) {
+//       case 'c':
+//         *str++ = va_arg(ap, int);
+//         break;
+//       case 'd':
+//         // cannot handle %02d etc. maybe fix future.
+//         str = num2str(str, va_arg(ap, ll), 10);
+//         break;
+//       case 'p':
+//         str = num2str(str, va_arg(ap, ll), 16);
+//         break;
+//       case 's':
+//         s = va_arg(ap, char *);
+//         while (*s) *str++ = *s++;
+//         break;
+//       default:
+//         if (*fmt != '%') *str++ = '%';
+//         if (*fmt) *str++ = *fmt;
+//         else fmt--;
+//         break;
+//     }
+//   }
+//   *str = '\0';
+//   return str - out;
+// }
+
+// int snprintf(char *out, size_t n, const char *fmt, ...) {
+//   panic("Not implemented");
+// }
+
+// int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+//   panic("Not implemented");
+// }
+
+// #endif
