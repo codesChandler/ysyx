d261b52bcbb68857525d2a1e401d8d6041ef9689
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-09 22:08:29 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:08:29 up  3:59,  1 user,  load average: 1.84, 0.90, 0.70

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,77 +1,76 @@
-#include <am.h>
-#include <klib.h>
-#include <klib-macros.h>
-#include <stdarg.h>
+// #include <am.h>
+// #include <klib.h>
+// #include <klib-macros.h>
+// #include <stdarg.h>
 
-#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-void itoa(unsigned int n, char * buf)
-{ int len,m;
-  for(len=0,m=n;m/10!=0;len++)
-    m/=10;
-  for(int i=0;i<=len;i++){
-    *(buf+len-i)=n%10+'0';
-    n=n/10;
-  }
-  // printf("len:%d\n",len);
-  *(buf+len+1)='\0';
-}
-
-// void input_decoder(va_list ap,char *out, const char *fmt){
-//   char *str = out;
-//   int d;
-//   char *s,buf[100];
-
-//   while (*fmt){
-//     if((*fmt)=='%'){
-//     switch (*(++fmt))
-//     {
-//     case 's': /* string */
-//       s = va_arg(ap, char *);
-//       while(*s) {*str=*s;str++;s++;}
-//       break;
-//     case 'c': /* string */
-//       *str = (char)va_arg(ap, int);
-//       str ++;
-//       break;    
-//     case 'd': /* int */
-//       d = va_arg(ap, int);
-//       if (d < 0)
-//       {
-//         *str = '-';
-//         str++;
-//         d = -d;
-//       }
-//       // printf("case d n=[%d]\n", d);
-//       itoa(d, buf);
-//       memcpy(str, buf, strlen(buf));
-//       str += strlen(buf);
-//       break;}fmt++;}
-//     else {*str=*fmt;
-//     str++;fmt++;}}
-//     *str='\0';
-  
-//   va_end(ap);
+// void itoa(unsigned int n, char * buf)
+// { int len,m;
+//   for(len=0,m=n;m/10!=0;len++)
+//     m/=10;
+//   for(int i=0;i<=len;i++){
+//     *(buf+len-i)=n%10+'0';
+//     n=n/10;
+//   }
+//   // printf("len:%d\n",len);
+//   *(buf+len+1)='\0';
 // }
 
-int printf(const char *fmt, ...) {
-  char out[100];
-  va_list ap;
-  va_start(ap, fmt);
-  int n=vsprintf(out,fmt,ap);
-  va_end(ap);
-  for(int i=0;i<strlen(out);i++)
-    putch(*(out+i));
-  return n;
-  // return 0;
-}
+// // void input_decoder(va_list ap,char *out, const char *fmt){
+// //   char *str = out;
+// //   int d;
+// //   char *s,buf[100];
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+// //   while (*fmt){
+// //     if((*fmt)=='%'){
+// //     switch (*(++fmt))
+// //     {
+// //     case 's': /* string */
+// //       s = va_arg(ap, char *);
+// //       while(*s) {*str=*s;str++;s++;}
+// //       break;
+// //     case 'c': /* string */
+// //       *str = (char)va_arg(ap, int);
+// //       str ++;
+// //       break;    
+// //     case 'd': /* int */
+// //       d = va_arg(ap, int);
+// //       if (d < 0)
+// //       {
+// //         *str = '-';
+// //         str++;
+// //         d = -d;
+// //       }
+// //       // printf("case d n=[%d]\n", d);
+// //       itoa(d, buf);
+// //       memcpy(str, buf, strlen(buf));
+// //       str += strlen(buf);
+// //       break;}fmt++;}
+// //     else {*str=*fmt;
+// //     str++;fmt++;}}
+// //     *str='\0';
+  
+// //   va_end(ap);
+// // }
+
+// int printf(const char *fmt, ...) {
+//   char out[100];
+//   va_list ap;
+//   va_start(ap, fmt);
+//   int n=vsprintf(out,fmt,ap);
+//   va_end(ap);
+//   for(int i=0;i<strlen(out);i++)
+//     putch(*(out+i));
+//   return n;
+//   // return 0;
+// }
+
+// int vsprintf(char *out, const char *fmt, va_list ap) {
 //   char *str = out;
 //   int d;
 //   char *s,buf[100];
-//   //char *fmt;//=fmt_t;
-//   //fmt=fmt_t;
+
 //   while (*fmt){
 //     if((*fmt)=='%'){
 //     switch (*(++fmt))
@@ -103,7 +102,78 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   
 //   va_end(ap);
 // return str-out;//strlen(out);
-char *s;
+// }
+
+
+// int sprintf(char *out, const char *fmt, ...)
+// {
+//   va_list ap;
+//   va_start(ap, fmt);
+//   int n=vsprintf(out,fmt,ap);
+//   va_end(ap);
+//   return n;
+//   // panic("Not implemented");
+// }
+
+// int snprintf(char *out, size_t n, const char *fmt, ...) {
+//   panic("Not implemented");
+// }
+
+// int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+//   panic("Not implemented");
+// }
+
+// #endif
+#include <am.h>
+#include <klib.h>
+#include <klib-macros.h>
+#include <stdarg.h>
+
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+typedef long long ll;
+
+char* num2str(char *str, ll num, ll base) {
+  char tmp[32];
+  if (num < 0) {
+    *str++ = '-';
+    num = -num;
+  }
+  int len = 0;
+  if (num == 0) tmp[len++] = 0;
+  else while (num) {
+    tmp[len++] = num % base;
+    num = num / base;
+  }
+  while (len-- > 0) {
+    if (tmp[len] < 10) *str++ = tmp[len] + '0';
+    else *str++ = tmp[len] - 10 + 'A';
+  }
+  return str;
+}
+
+int printf(const char *fmt, ...) {
+  int n;
+  char buf[8192];
+  va_list args;
+  va_start(args, fmt);
+  n = vsprintf(buf, fmt, args);
+  va_end(args);
+  putstr(buf);
+  return n;
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  int n;
+  va_list args;
+  va_start(args, fmt);
+  n = vsprintf(out, fmt, args);
+  va_end(args);
+  return n;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  char *s;
   char *str;
   for (str = out; *fmt; fmt++) {
     if (*fmt != '%') {
@@ -115,13 +185,13 @@ char *s;
       case 'c':
         *str++ = va_arg(ap, int);
         break;
-      // case 'd':
-      //   // cannot handle %02d etc. maybe fix future.
-      //   str = num2str(str, va_arg(ap, ll), 10);
-      //   break;
-      // case 'p':
-      //   str = num2str(str, va_arg(ap, ll), 16);
-      //   break;
+      case 'd':
+        // cannot handle %02d etc. maybe fix future.
+        str = num2str(str, va_arg(ap, ll), 10);
+        break;
+      case 'p':
+        str = num2str(str, va_arg(ap, ll), 16);
+        break;
       case 's':
         s = va_arg(ap, char *);
         while (*s) *str++ = *s++;
@@ -135,17 +205,6 @@ char *s;
   }
   *str = '\0';
   return str - out;
-}
-
-
-int sprintf(char *out, const char *fmt, ...)
-{
-  va_list ap;
-  va_start(ap, fmt);
-  int n=vsprintf(out,fmt,ap);
-  va_end(ap);
-  return n;
-  // panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
