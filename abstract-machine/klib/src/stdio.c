0489b670aacd39565f7ca3cfd74a29d6d576ca20
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-10 12:31:05 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:31:05 up  2:37,  1 user,  load average: 1.04, 0.52, 0.20

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -18,7 +18,7 @@ void itoa(unsigned int n, char * buf)
 }
 
 int printf(const char *fmt, ...) {
-  char out[8096];
+  char out[100];
   va_list ap;
   va_start(ap, fmt);
   int n=vsprintf(out,fmt,ap);
