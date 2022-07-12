0c89793d09b501436f5afc729b1f09d3062eb37a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 22:26:54 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:26:54 up 28 min,  1 user,  load average: 0.43, 0.44, 0.37

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "stdio.h"
 
 #ifdef __ISA_NATIVE__
 #error can not support ISA=native
@@ -8,6 +9,6 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  prtinf("I am here\n");
+  printf("I am here\n");
   return _syscall_(SYS_yield, 0, 0, 0);
 }
