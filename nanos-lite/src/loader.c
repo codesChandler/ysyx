cdaa3b40059e0b73a616fd811c1e2d0ed79312eb
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 21:08:56 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:08:56 up  4:51,  1 user,  load average: 0.96, 0.75, 0.66

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -56,7 +56,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = (loader(pcb, filename) & 0xFFFFFFFF);
-  Log("Jump to entry = %lx", entry);
+  Log("Jump to entry = %x", entry);
   ((void(*)())entry) ();
 }
 
