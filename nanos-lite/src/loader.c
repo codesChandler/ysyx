14298041157a1e6e8454be5cef07be81530a66a0
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 19:47:37 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:47:37 up  3:30,  1 user,  load average: 0.91, 0.72, 0.54

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,22 +16,23 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
   int elf_size=get_ramdisk_size();
-  uint8_t *buf[elf_size];
-  assert(ramdisk_read(buf,0,elf_size)==elf_size);
-  Elf_Ehdr *Ehdr=(void *)buf;
-  int phentsize=Ehdr->e_phentsize;
-  int phennum=Ehdr->e_phnum;
-  Elf_Phdr *Phdr[phennum];
-  assert(ramdisk_read(Phdr,Ehdr-> e_ehsize,phentsize*phennum)==phentsize*phennum);
-  for(int i=0;i<phennum;i++){
-    if(Phdr[i]-> p_type == PT_LOAD){
-      uint8_t *pbuf=(uint8_t *)malloc(Phdr[i]->p_filesz);
-      assert(ramdisk_read(pbuf,Phdr[i]->p_offset,Phdr[i]->p_filesz)==Phdr[i]->p_filesz);
-      free(pbuf);
-    }
-  }
-  while(1){}
-  return Ehdr->e_entry;
+  // uint8_t *buf[elf_size];
+  // assert(ramdisk_read(buf,0,elf_size)==elf_size);
+  // Elf_Ehdr *Ehdr=(void *)buf;
+  // int phentsize=Ehdr->e_phentsize;
+  // int phennum=Ehdr->e_phnum;
+  // Elf_Phdr *Phdr[phennum];
+  // assert(ramdisk_read(Phdr,Ehdr-> e_ehsize,phentsize*phennum)==phentsize*phennum);
+  // for(int i=0;i<phennum;i++){
+  //   if(Phdr[i]-> p_type == PT_LOAD){
+  //     uint8_t *pbuf=(uint8_t *)malloc(Phdr[i]->p_filesz);
+  //     assert(ramdisk_read(pbuf,Phdr[i]->p_offset,Phdr[i]->p_filesz)==Phdr[i]->p_filesz);
+  //     free(pbuf);
+  //   }
+  // }
+  while(1);
+  return elf_size;
+  // return Ehdr->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
