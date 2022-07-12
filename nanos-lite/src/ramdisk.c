c0e6a7a11403d41e3de0536c230cbe440b11836a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 19:49:01 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:49:01 up  3:31,  1 user,  load average: 1.62, 0.95, 0.63

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -12,6 +12,7 @@ extern uint8_t ramdisk_end;
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
+  printf("I am back0\n");
   memcpy(buf, &ramdisk_start + offset, len);
   printf("I am back\n");
   return len;
