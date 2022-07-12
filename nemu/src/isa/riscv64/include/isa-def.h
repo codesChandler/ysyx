27a8a37a5c2adbfe9a4b4837ad1ab09a61f8beae
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 11:28:44 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:28:44 up  1:29,  1 user,  load average: 0.38, 0.31, 0.26

--- nemu/src/isa/riscv64/include/isa-def.h
+++ nemu/src/isa/riscv64/include/isa-def.h
@@ -20,7 +20,7 @@ typedef struct {
 #define MSTATUS_INDEX 0x300
 #define MCAUSE_INDEX 0x342
 
-#define En_Call_M 0xb //Environment call from M-mode
+#define Env_Call_M 0xb //Environment call from M-mode
 
 // decode
 typedef struct {
