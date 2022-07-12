ebe48fa2e6db8732f2341aab62e2796aa7c33753
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 19:38:09 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:38:09 up  3:20,  1 user,  load average: 0.48, 0.53, 0.40

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,21 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern size_t get_ramdisk_size();
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  // TODO();
+  int elf_size=get_ramdisk_size();
+  uint8_t *buf[elf_size];
+  assert(ramdisk_read(buf,0,elf_size)==elf_size);
+  Elf_Ehdr *Ehdr=(void *)buf;
+  int phentsize=Ehdr->e_phentsize;
+  int phennum=Ehdr->e_phnum;
+  Elf_Phdr *Phdr[phennum];
+  assert(ramdisk_read(Phdr,Ehdr-> e_ehsize,phentsize*phennum)==phentsize*phennum);
+  
+  return Ehdr->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
