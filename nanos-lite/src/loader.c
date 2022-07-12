5108d70737c00cd083c8b22b95d50de238130f11
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 20:50:45 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:50:45 up  4:33,  1 user,  load average: 0.51, 0.42, 0.48

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -42,8 +42,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       assert(ramdisk_read(pbuf,Phdr[i].p_offset,Phdr[i].p_filesz)==Phdr[i].p_filesz);
       assert(Phdr[i].p_vaddr!=0);
       memcpy((void *)Phdr[i].p_vaddr,pbuf,Phdr[i].p_filesz);
+      printf("0\n");
       assert(Phdr[i].p_vaddr+Phdr[i].p_filesz!=0);
       memcpy((void *)Phdr[i].p_vaddr+Phdr[i].p_filesz,0,Phdr[i].p_memsz-Phdr[i].p_filesz);
+      printf("1\n");
     }
   }
   // return elf_size;
