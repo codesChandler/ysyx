390ce97e8b1901b58ab3ea5be67d03f2f2d69758
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 20:28:14 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:28:14 up  4:10,  1 user,  load average: 1.26, 0.84, 0.67

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -54,7 +54,7 @@ void *malloc(size_t size) {
     char *old = malloc_ptr;
     malloc_ptr += size;
   // return old;
-    printf("heap.start:%d--malloc_ptr:%d\n",heap.start,malloc_ptr);
+    printf("heap.start:%d--malloc_ptr:%d--heap.end:%d\n",heap.start,malloc_ptr,heap.end);
     assert(((uintptr_t)heap.start <= (uintptr_t)malloc_ptr));
     // assert(((uintptr_t)malloc_ptr< (uintptr_t)heap.end));
  
