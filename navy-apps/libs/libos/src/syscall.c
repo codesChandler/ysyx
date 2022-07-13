402f05a00a3b3a1e4342fa4918717f3dd348029d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 22:08:25 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:08:25 up 12:03,  1 user,  load average: 0.45, 0.59, 0.62

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -61,26 +61,25 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _syscall_(SYS_brk,0, 0, 0);
   return _syscall_(SYS_write, fd, (int)buf, count);;//write返回值需要查man
 }
 
 extern char end;
 void *_sbrk(intptr_t increment) {
-  _syscall_(SYS_brk,0, 0, 0);
-  // static intptr_t pgb=&end;
-  // intptr_t old=pgb;
-  // intptr_t new=pgb+increment;
-  // char buf[100];
-  // sprintf(buf,"I am here\n");
+  // _syscall_(SYS_brk,0, 0, 0);
+  static intptr_t pgb=&end;
+  intptr_t old=pgb;
+  intptr_t new=pgb+increment;
+  char buf[100];
+  // // sprintf(buf,"I am here\n");
   // _write(1, buf, 10);
-  // if(1){//!_syscall_(SYS_brk,0, 0, 0)){
-  //   sprintf(buf,"I am here\n");
-  //   _write(1, buf, 10);
-  //   pgb=new;
-  //   return (void *)old;}
-  // else return (void *)-1;
-  return (void *)-1;
+  if(1){//!_syscall_(SYS_brk,0, 0, 0)){
+    sprintf(buf,"I am here\n");
+    _write(1, buf, 10);
+    pgb=new;
+    return (void *)old;}
+  else return (void *)-1;
+  // return (void *)-1;
 }
 
 int _read(int fd, void *buf, size_t count) {
