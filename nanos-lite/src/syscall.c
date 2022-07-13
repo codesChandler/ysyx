7ab224c22408f69d4c6584df4acebdb009a6781e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 20:46:01 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:46:01 up 10:40,  1 user,  load average: 0.66, 0.59, 0.64

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,11 +14,8 @@ void sys_exit(Context *c){
 
 int sys_write(Context *c){ 
   int fd=c->GPR2;
-  // printf("fd:%d\n",fd);
   uintptr_t buf=c->GPR3;
-  // printf("buf:%x\n",buf);
   int len=c->GPR4;
-  // printf("len:%d\n",len);
   int i=0;
   if(fd==1 || fd==2){
     for(;i<len;i++){
