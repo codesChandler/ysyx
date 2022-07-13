5a9cd28f779f3884a049d0feb73db35270967f52
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 17:43:57 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:43:57 up  7:38,  1 user,  load average: 1.08, 0.46, 0.33

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,6 @@
 #include <common.h>
 #include "syscall.h"
-
+#define CONFIG_STRACE 1
 int sys_yield(){
   yield();
   return 0;
@@ -13,6 +13,10 @@ void sys_exit(Context *c){
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  
+  #ifdef CONFIG_STRACE
+      Log("syscall ID= %d", a[0]);
+  #endif
 
   switch (a[0]) {
     case SYS_yield:c->GPRx=sys_yield();break;
