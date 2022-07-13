f4a638270bd15b08fea5abfb38094edd241192a4
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 21:45:39 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:45:39 up 11:40,  1 user,  load average: 0.00, 0.11, 0.14

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -24,6 +24,10 @@ int sys_write(Context *c){
   return i;
 }
 
+int sys_brk(){
+  return 0;
+}
+
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -36,6 +40,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_yield:c->GPRx=sys_yield();break;
     case SYS_exit:sys_exit(c);break;
+    case SYS_brk:c->GPRx=sys_brk();break;
     case SYS_write:c->GPRx=sys_write(c);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
