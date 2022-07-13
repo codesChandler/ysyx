4dc600d306a251b3d75445b432b3414fc9daaa5e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 17:33:38 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:33:38 up  7:28,  1 user,  load average: 1.12, 0.69, 0.36

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,7 +4,7 @@ extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_SYSCALL:do_syscall(c);break;
-    case EVENT_YIELD:Log("EVENT_YIELD\n");
+    case EVENT_YIELD:Log("EVENT_YIELD\n");break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
