f071cfb0091088d44545283ae0392044a4c8f556
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 22:54:17 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:54:17 up 13:55,  1 user,  load average: 0.97, 0.81, 0.62

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -59,9 +59,9 @@ void do_syscall(Context *c) {
 
   extern void Log_(int ID,int fd);
   #ifdef CONFIG_STRACE
-        if((a[0]==2||a[0]==3||a[0]==4||a[0]==7||a[0]==8))
-          Log_(a[0],c->GPR2);
-        else
+        // if((a[0]==2||a[0]==3||a[0]==4||a[0]==7||a[0]==8))
+        //   Log_(a[0],c->GPR2);
+        // else
           Log("syscall ID= %s", syscall_name[a[0]]);
   #endif
 
