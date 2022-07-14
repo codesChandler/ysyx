3c3f0766f0457d4576f94c74a3f286d26cab0a8e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 13:50:11 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:50:11 up  4:51,  1 user,  load average: 0.38, 0.48, 0.39

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -53,7 +53,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     if(Phdr[i].p_type == PT_LOAD){
       // ramdisk_read((void *)Phdr[i].p_vaddr,Phdr[i].p_offset,Phdr[i].p_filesz);//不明白为啥采用buf传递就不对，可能与内存存放，地址对齐有关
       // fs_read(fd, (void *)Phdr[i].p_vaddr, Phdr[i].p_filesz);
-    
+      assert(fs_lseek(fd,Phdr[i].p_offset,SEEK_SET)==Phdr[i].p_offset);
+      assert(fs_read(fd,(void *)Phdr[i].p_vaddr,Phdr[i].p_filesz)==Phdr[i].p_filesz);
       // ramdisk_read((void *)pbuf,Phdr[i].p_offset,Phdr[i].p_filesz);
       // memcpy((void *)Phdr[i].p_vaddr,(void *)pbuf,Phdr[i].p_filesz);
       memset((void *)Phdr[i].p_vaddr+Phdr[i].p_filesz,0,Phdr[i].p_memsz-Phdr[i].p_filesz);
