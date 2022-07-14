b539845156f023e083a23fe8dbfae262edf68b5c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 16:01:07 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:01:07 up  7:02,  1 user,  load average: 1.98, 1.34, 0.72

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
-
+extern int _open(const char *path, int flags, int mode);
 int main() {
   printf("I am here\n");
-  FILE *fp = fopen("/share/files/num", "r+");
+  FILE *fp = _open("/share/files/num", '0','0');
   assert(fp);
 
   // fseek(fp, 0, SEEK_END);
