5fe9001130ff06094ff87c60f97d83ac46a8024c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 13:37:06 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:37:06 up  4:38,  1 user,  load average: 1.15, 0.57, 0.30

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -38,8 +38,9 @@ void init_fs() {
 
 int fs_open(const char *pathname, int flags, int mode){
   for(int i=0;i<sizeof(file_table);i++){
-    if(strcmp(pathname,file_table[i].name)==0)
-      return i;
+    if(strcmp(pathname,file_table[i].name)==0){
+      file_table[i].open_offset=0;
+      return i;}
   }
   assert(0);
 }
