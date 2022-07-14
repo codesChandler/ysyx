9aa9efb783360579343b4cbe697688350be6b88e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 17:05:16 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:05:16 up  8:06,  1 user,  load average: 0.35, 0.52, 0.57

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,83 +1,3 @@
-// #include <fs.h>
-
-// typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
-// typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
-
-// typedef struct {
-//   char *name;
-//   size_t size;
-//   size_t disk_offset;
-//   size_t open_offset;
-//   ReadFn read;
-//   WriteFn write;
-// } Finfo;
-
-// enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
-
-// size_t invalid_read(void *buf, size_t offset, size_t len) {
-//   panic("should not reach here");
-//   return 0;
-// }
-
-// size_t invalid_write(const void *buf, size_t offset, size_t len) {
-//   panic("should not reach here");
-//   return 0;
-// }
-
-// /* This is the information about all files in disk. */
-// static Finfo file_table[] __attribute__((used)) = {//文件记录表
-//   [FD_STDIN]  = {"stdin", 0, 0, 0,invalid_read, invalid_write},
-//   [FD_STDOUT] = {"stdout", 0, 0,0, invalid_read, invalid_write},
-//   [FD_STDERR] = {"stderr", 0, 0,0, invalid_read, invalid_write},
-// #include "files.h"
-// };
-
-// void init_fs() {
-//   // TODO: initialize the size of /dev/fb
-// }
-
-// int fs_open(const char *pathname, int flags, int mode){
-//   for(int i=0;i<sizeof(file_table);i++){
-//     if(strcmp(pathname,file_table[i].name)==0){
-//       file_table[i].open_offset=0;
-//       return i;}
-//   }
-//   assert(0);
-// }
-
-// extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
-
-// size_t fs_read(int fd, void *buf, size_t len){
-//   printf("file_table[fd].open_offset:%d\n",file_table[fd].open_offset);
-//   assert((file_table[fd].open_offset+len)<=file_table[fd].size);
-//   ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
-//   file_table[fd].open_offset+=len;
-//   return len;
-// }
-
-// // size_t fs_write(int fd, const void *buf, size_t len){
-// //   printf("I am here\n");
-// //   return 0;
-// // }
-
-// int fs_size(int fd){
-//   return file_table[fd].size;
-// }
-
-// //enum {SEEK_SET,SEEK_CUR,SEEK_end};
-
-// size_t fs_lseek(int fd, size_t offset, int whence){
-//   if(whence == SEEK_SET) file_table[fd].open_offset=offset;
-//   else if(whence == SEEK_CUR) file_table[fd].open_offset+=offset;
-//   else file_table[fd].open_offset = file_table[fd].size;
-
-//   return file_table[fd].open_offset;
-// }
-
-// int fs_close(int fd){
-//   return 0;
-// }
-
 #include <fs.h>
 
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
@@ -87,12 +7,12 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
-  size_t f_offset;
 } Finfo;
 
-enum {FD_STDIN=0, FD_STDOUT, FD_STDERR, FD_FB};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -104,77 +24,56 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
-size_t serial_write(const void *buf, size_t offset, size_t len);
 /* This is the information about all files in disk. */
-static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
+static Finfo file_table[] __attribute__((used)) = {//文件记录表
+  [FD_STDIN]  = {"stdin", 0, 0, 0,invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0,0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0,0, invalid_read, invalid_write},
 #include "files.h"
 };
 
-int f_num = sizeof(file_table)/sizeof(file_table[0]);
+void init_fs() {
+  // TODO: initialize the size of /dev/fb
+}
+
 int fs_open(const char *pathname, int flags, int mode){
-  int i;
-  int fd = -1;
-  for(i=0;i<f_num;i++){
-    fd = strcmp(file_table[i].name,pathname);
-    if(fd==0){
-      file_table[i].f_offset = 0;
-      printf("The strace is SYS_open,The open file is %s\n",file_table[fd].name);
-      return i;
-    }
+  for(int i=0;i<sizeof(file_table);i++){
+    if(strcmp(pathname,file_table[i].name)==0){
+      file_table[i].open_offset=0;
+      return i;}
   }
-  printf("pathname %s is not existing\n",file_table[i].name);
   assert(0);
 }
 
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 size_t fs_read(int fd, void *buf, size_t len){
-  int len_ = 0;
-  if(len==0 || file_table[fd].f_offset>=file_table[fd].size)
-    return 0;
-  if(file_table[fd].f_offset+len>file_table[fd].size)
-    len = file_table[fd].size-file_table[fd].f_offset;
-  len_ = ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].f_offset,len);
-  file_table[fd].f_offset += len; 
-  return len_;
+  printf("file_table[fd].open_offset:%d\n",file_table[fd].open_offset);
+  assert((file_table[fd].open_offset+len)<=file_table[fd].size);
+  ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
+  file_table[fd].open_offset+=len;
+  return len;
 }
 
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-size_t fs_write(int fd, const void *buf, size_t len){
-  int len_ = len;
-    if(file_table[fd].write !=NULL){
-      len_ = file_table[fd].write(buf,file_table[fd].disk_offset+file_table[fd].f_offset,len_);
-    }else{
-      if(len_==0 || file_table[fd].f_offset>=file_table[fd].size)
-        assert(0);
-      if(file_table[fd].f_offset+len>file_table[fd].size)
-        len_ = file_table[fd].size-file_table[fd].f_offset;
-      len_ = ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].f_offset,len_);
-    }  
-    file_table[fd].f_offset += len_; 
-    return len_;
+// size_t fs_write(int fd, const void *buf, size_t len){
+//   printf("I am here\n");
+//   return 0;
+// }
+
+int fs_size(int fd){
+  return file_table[fd].size;
+}
+
+//enum {SEEK_SET,SEEK_CUR,SEEK_end};
+
+size_t fs_lseek(int fd, size_t offset, int whence){
+  if(whence == SEEK_SET) file_table[fd].open_offset=offset;
+  else if(whence == SEEK_CUR) file_table[fd].open_offset+=offset;
+  else file_table[fd].open_offset = file_table[fd].size;
+
+  return file_table[fd].open_offset;
 }
 
 int fs_close(int fd){
   return 0;
-}
-
-size_t fs_lseek(int fd, size_t offset, int whence){
-  switch(whence){
-    case SEEK_SET:file_table[fd].f_offset = offset;break;
-    case SEEK_CUR:file_table[fd].f_offset += offset;break;
-    case SEEK_END:file_table[fd].f_offset = file_table[fd].size+ offset;break;
-    default:file_table[fd].f_offset = -1;break;
-  }
-  if(file_table[fd].f_offset>file_table[fd].size)
-    file_table[fd].f_offset = -1;
-  return file_table[fd].f_offset;
-}
-
-
-
-void init_fs() {
-  // TODO: initialize the size of /dev/fb
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,80 +1,7 @@
-// #include <proc.h>
-// #include <elf.h>
-// #include "fs.h"
-
-// #ifdef __LP64__
-// # define Elf_Ehdr Elf64_Ehdr
-// # define Elf_Phdr Elf64_Phdr
-// #else
-// # define Elf_Ehdr Elf32_Ehdr
-// # define Elf_Phdr Elf32_Phdr
-// #endif
-
-// extern size_t get_ramdisk_size();
-// extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
-
-// extern int fs_open(const char *pathname, int flags, int mode);
-// extern size_t fs_read(int fd, void *buf, size_t len);
-// extern int fs_size(int fd);
-// extern size_t fs_lseek(int fd, size_t offset, int whence);
-
-// static uintptr_t loader(PCB *pcb, const char *filename) {
-//   int fd=fs_open(filename, 0, 0);
-
-//   int elf_size=fs_size(fd);
-//   uint8_t buf[elf_size];
-
-//   int rlen=fs_read(fd,buf,elf_size);
-//   assert(rlen==elf_size);
-
-//   Elf_Ehdr *Ehdr=(void *)buf;
-//   assert(*(uint32_t *)(Ehdr->e_ident) == 0x464C457f);
-
-//   assert(Ehdr->e_machine == EXPECT_TYPE);
-
-//   int phentsize=Ehdr->e_phentsize;
-//   int phennum=Ehdr->e_phnum;
-
-//   Elf_Phdr Phdr[phennum];
-//   assert(fs_lseek(fd,Ehdr-> e_ehsize,SEEK_SET)==Ehdr-> e_ehsize);
-//   assert(fs_read(fd,Phdr,phentsize*phennum)==phentsize*phennum);
-
-
-//   // int filesz=0;
-//   // for(int i=0;i<phennum;i++){//find max size
-//   //   if(filesz<Phdr[i].p_filesz) filesz=Phdr[i].p_filesz;
-//   // }
-//   // printf("p_filesz:%d\n",filesz);
-//   // uint8_t pbuf[256];//256可以运行成功，257失败
-//   // uint8_t *bbuf=pbuf;
-
-//   for(int i=0;i<phennum;i++){
-
-//     if(Phdr[i].p_type == PT_LOAD){
-//       // ramdisk_read((void *)Phdr[i].p_vaddr,Phdr[i].p_offset,Phdr[i].p_filesz);//不明白为啥采用buf传递就不对，可能与内存存放，地址对齐有关
-//       // fs_read(fd, (void *)Phdr[i].p_vaddr, Phdr[i].p_filesz);
-//       assert(fs_lseek(fd,Phdr[i].p_offset,SEEK_SET)==Phdr[i].p_offset);
-//       assert(fs_read(fd,(void *)Phdr[i].p_vaddr,Phdr[i].p_filesz)==Phdr[i].p_filesz);
-//       // ramdisk_read((void *)pbuf,Phdr[i].p_offset,Phdr[i].p_filesz);
-//       // memcpy((void *)Phdr[i].p_vaddr,(void *)pbuf,Phdr[i].p_filesz);
-//       memset((void *)Phdr[i].p_vaddr+Phdr[i].p_filesz,0,Phdr[i].p_memsz-Phdr[i].p_filesz);
-
-//     }
-//   }
-
-//   return (uint32_t)Ehdr->e_entry;
-// }
-
-// void naive_uload(PCB *pcb, const char *filename) {
-//   //uintptr_t entry = (unsigned)(loader(pcb, filename) & 0xFFFFFFFF);
-//   uintptr_t entry = loader(pcb, filename);
-//   Log("Jump to entry = 0x%p", entry);
-//   ((void(*)())entry) ();
-// }
-
 #include <proc.h>
 #include <elf.h>
-#include <fs.h>
+#include "fs.h"
+
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -83,47 +10,65 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern size_t get_ramdisk_size();
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
-int    fs_open(const char *pathname, int flags, int mode);
-size_t fs_read(int fd, void *buf, size_t len);
-int    fs_close(int fd);
-size_t fs_lseek(int fd, size_t offset, int whence);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern size_t fs_read(int fd, void *buf, size_t len);
+extern int fs_size(int fd);
+extern size_t fs_lseek(int fd, size_t offset, int whence);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  #if defined(__ISA_AM_NATIVE__)
-  # define EXPECT_TYPE EM_X86_64
-  #elif defined(__ISA_X86__)
-  # define EXPECT_TYPE EM_X86_64
-  #elif defined(__ISA_MIPS32__)
-  # define EXPECT_TYPE EM_MIPS_X
-  #elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
-  # define EXPECT_TYPE EM_RISCV
-  #else
-  # error Unsupported ISA
-  #endif
-  int fd = fs_open(filename,0,0);
-  Elf_Ehdr elf;
-  fs_read(fd,&elf,sizeof(elf));
-  assert( *(uint32_t *)elf.e_ident == 0x464c457f);
-  assert(elf.e_machine == EXPECT_TYPE);
-  int i; 
-  for(i=0;i<elf.e_phnum;i++){
-    Elf_Phdr ph;
-    fs_lseek(fd,elf.e_phoff + i*elf.e_phentsize,SEEK_SET);
-    fs_read(fd,&ph,elf.e_phentsize);
-    if(ph.p_type==PT_LOAD){
-      fs_lseek(fd,ph.p_offset,SEEK_SET);
-      fs_read(fd,(void *)ph.p_vaddr,ph.p_memsz);
-      memset((void *)(ph.p_vaddr+ph.p_filesz),0,ph.p_memsz-ph.p_filesz);
+  int fd=fs_open(filename, 0, 0);
+
+  int elf_size=fs_size(fd);
+  uint8_t buf[elf_size];
+
+  int rlen=fs_read(fd,buf,elf_size);
+  assert(rlen==elf_size);
+
+  Elf_Ehdr *Ehdr=(void *)buf;
+  assert(*(uint32_t *)(Ehdr->e_ident) == 0x464C457f);
+
+  assert(Ehdr->e_machine == EXPECT_TYPE);
+
+  int phentsize=Ehdr->e_phentsize;
+  int phennum=Ehdr->e_phnum;
+
+  Elf_Phdr Phdr[phennum];
+  assert(fs_lseek(fd,Ehdr-> e_ehsize,SEEK_SET)==Ehdr-> e_ehsize);
+  assert(fs_read(fd,Phdr,phentsize*phennum)==phentsize*phennum);
+
+
+  // int filesz=0;
+  // for(int i=0;i<phennum;i++){//find max size
+  //   if(filesz<Phdr[i].p_filesz) filesz=Phdr[i].p_filesz;
+  // }
+  // printf("p_filesz:%d\n",filesz);
+  // uint8_t pbuf[256];//256可以运行成功，257失败
+  // uint8_t *bbuf=pbuf;
+
+  for(int i=0;i<phennum;i++){
+
+    if(Phdr[i].p_type == PT_LOAD){
+      // ramdisk_read((void *)Phdr[i].p_vaddr,Phdr[i].p_offset,Phdr[i].p_filesz);//不明白为啥采用buf传递就不对，可能与内存存放，地址对齐有关
+      // fs_read(fd, (void *)Phdr[i].p_vaddr, Phdr[i].p_filesz);
+      assert(fs_lseek(fd,Phdr[i].p_offset,SEEK_SET)==Phdr[i].p_offset);
+      assert(fs_read(fd,(void *)Phdr[i].p_vaddr,Phdr[i].p_filesz)==Phdr[i].p_filesz);
+      // ramdisk_read((void *)pbuf,Phdr[i].p_offset,Phdr[i].p_filesz);
+      // memcpy((void *)Phdr[i].p_vaddr,(void *)pbuf,Phdr[i].p_filesz);
+      memset((void *)Phdr[i].p_vaddr+Phdr[i].p_filesz,0,Phdr[i].p_memsz-Phdr[i].p_filesz);
+
     }
   }
-  //printf("%d\n",elf.e_entry);
-  fs_close(fd);
-  return elf.e_entry;
+
+  return (uint32_t)Ehdr->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb,filename );
-  Log("Jump to entry = %p", entry);
+  //uintptr_t entry = (unsigned)(loader(pcb, filename) & 0xFFFFFFFF);
+  uintptr_t entry = loader(pcb, filename);
+  Log("Jump to entry = 0x%p", entry);
   ((void(*)())entry) ();
 }
+
