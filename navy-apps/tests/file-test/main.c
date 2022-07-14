c422a111b0a87bd802ac9aa6f38d94d5bf2d2b61
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 16:45:50 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:45:50 up  7:46,  1 user,  load average: 0.08, 0.27, 0.49

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -5,7 +5,7 @@
 int main() {
   printf("I am in file-test:%d\n",_syscall_(SYS_open, (int)&"/share/files/num", 0, 0));
 
-  FILE *fp = open("/share/files/num", "r+");
+  FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
 
   // fseek(fp, 0, SEEK_END);
