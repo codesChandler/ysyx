4a38822e5781f9011bb4a25bd3e395ceefd0ce26
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 19:35:21 +0800

    > compile NEMU ysyx_22040632 向辉 Linux chandler-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:35:21 up 10:36,  1 user,  load average: 0.29, 0.48, 0.52

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,7 +1,70 @@
+// #include <proc.h>
+// #include <elf.h>
+// #include "fs.h"
+
+// #ifdef __LP64__
+// # define Elf_Ehdr Elf64_Ehdr
+// # define Elf_Phdr Elf64_Phdr
+// #else
+// # define Elf_Ehdr Elf32_Ehdr
+// # define Elf_Phdr Elf32_Phdr
+// #endif
+
+// extern size_t get_ramdisk_size();
+// extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
+// extern int fs_open(const char *pathname, int flags, int mode);
+// extern size_t fs_read(int fd, void *buf, size_t len);
+// extern int fs_size(int fd);
+// extern size_t fs_lseek(int fd, size_t offset, int whence);
+// extern int fs_close(int fd);
+
+// static uintptr_t loader(PCB *pcb, const char *filename) {
+//   int fd=fs_open(filename, 0, 0);
+
+//   int elf_size=fs_size(fd);
+//   printf("elf_size:%d\n",elf_size);
+//   uint8_t buf[elf_size];
+
+//   int rlen=fs_read(fd,buf,elf_size);
+//   assert(rlen==elf_size);
+
+//   Elf_Ehdr *Ehdr=(void *)buf;
+//   assert(*(uint32_t *)(Ehdr->e_ident) == 0x464C457f);
+
+//   assert(Ehdr->e_machine == EXPECT_TYPE);
+
+//   int phentsize=Ehdr->e_phentsize;
+//   int phennum=Ehdr->e_phnum;
+
+//   Elf_Phdr Phdr[phennum];
+//   assert(fs_lseek(fd,Ehdr-> e_ehsize,SEEK_SET)==Ehdr-> e_ehsize);
+//   assert(fs_read(fd,Phdr,phentsize*phennum)==phentsize*phennum);
+
+//   for(int i=0;i<phennum;i++){
+//     if(Phdr[i].p_type == PT_LOAD){
+//       printf("Phdr[i].p_offset:%d\n",Phdr[i].p_offset);
+//       assert(fs_lseek(fd,Phdr[i].p_offset,SEEK_SET)==Phdr[i].p_offset);
+//       assert(fs_read(fd,(void *)Phdr[i].p_vaddr,Phdr[i].p_filesz)==Phdr[i].p_filesz);
+
+//       memset((void *)Phdr[i].p_vaddr+Phdr[i].p_filesz,0,Phdr[i].p_memsz-Phdr[i].p_filesz);
+
+//     }
+//   }
+//   fs_close(fd);
+//   return (uint32_t)Ehdr->e_entry;
+// }
+
+// void naive_uload(PCB *pcb, const char *filename) {
+//   //uintptr_t entry = (unsigned)(loader(pcb, filename) & 0xFFFFFFFF);
+//   uintptr_t entry = loader(pcb, filename);
+//   Log("Jump to entry = 0x%p", entry);
+//   ((void(*)())entry) ();
+// }
+
 #include <proc.h>
 #include <elf.h>
-#include "fs.h"
-
+#include <fs.h>
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -10,55 +73,47 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-extern size_t get_ramdisk_size();
-extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
-extern int fs_open(const char *pathname, int flags, int mode);
-extern size_t fs_read(int fd, void *buf, size_t len);
-extern int fs_size(int fd);
-extern size_t fs_lseek(int fd, size_t offset, int whence);
-extern int fs_close(int fd);
+int    fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+int    fs_close(int fd);
+size_t fs_lseek(int fd, size_t offset, int whence);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  int fd=fs_open(filename, 0, 0);
-
-  int elf_size=fs_size(fd);
-  printf("elf_size:%d\n",elf_size);
-  uint8_t buf[elf_size];
-
-  int rlen=fs_read(fd,buf,elf_size);
-  assert(rlen==elf_size);
-
-  Elf_Ehdr *Ehdr=(void *)buf;
-  assert(*(uint32_t *)(Ehdr->e_ident) == 0x464C457f);
-
-  assert(Ehdr->e_machine == EXPECT_TYPE);
-
-  int phentsize=Ehdr->e_phentsize;
-  int phennum=Ehdr->e_phnum;
-
-  Elf_Phdr Phdr[phennum];
-  assert(fs_lseek(fd,Ehdr-> e_ehsize,SEEK_SET)==Ehdr-> e_ehsize);
-  assert(fs_read(fd,Phdr,phentsize*phennum)==phentsize*phennum);
-
-  for(int i=0;i<phennum;i++){
-    if(Phdr[i].p_type == PT_LOAD){
-      printf("Phdr[i].p_offset:%d\n",Phdr[i].p_offset);
-      assert(fs_lseek(fd,Phdr[i].p_offset,SEEK_SET)==Phdr[i].p_offset);
-      assert(fs_read(fd,(void *)Phdr[i].p_vaddr,Phdr[i].p_filesz)==Phdr[i].p_filesz);
-
-      memset((void *)Phdr[i].p_vaddr+Phdr[i].p_filesz,0,Phdr[i].p_memsz-Phdr[i].p_filesz);
-
+  #if defined(__ISA_AM_NATIVE__)
+  # define EXPECT_TYPE EM_X86_64
+  #elif defined(__ISA_X86__)
+  # define EXPECT_TYPE EM_X86_64
+  #elif defined(__ISA_MIPS32__)
+  # define EXPECT_TYPE EM_MIPS_X
+  #elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
+  # define EXPECT_TYPE EM_RISCV
+  #else
+  # error Unsupported ISA
+  #endif
+  int fd = fs_open(filename,0,0);
+  Elf_Ehdr elf;
+  fs_read(fd,&elf,sizeof(elf));
+  assert( *(uint32_t *)elf.e_ident == 0x464c457f);
+  assert(elf.e_machine == EXPECT_TYPE);
+  int i; 
+  for(i=0;i<elf.e_phnum;i++){
+    Elf_Phdr ph;
+    fs_lseek(fd,elf.e_phoff + i*elf.e_phentsize,SEEK_SET);
+    fs_read(fd,&ph,elf.e_phentsize);
+    if(ph.p_type==PT_LOAD){
+      fs_lseek(fd,ph.p_offset,SEEK_SET);
+      fs_read(fd,(void *)ph.p_vaddr,ph.p_memsz);
+      memset((void *)(ph.p_vaddr+ph.p_filesz),0,ph.p_memsz-ph.p_filesz);
     }
   }
+  //printf("%d\n",elf.e_entry);
   fs_close(fd);
-  return (uint32_t)Ehdr->e_entry;
+  return elf.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  //uintptr_t entry = (unsigned)(loader(pcb, filename) & 0xFFFFFFFF);
-  uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = 0x%p", entry);
+  uintptr_t entry = loader(pcb,filename );
+  Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
 }
-
--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -1,13 +1,13 @@
 #include "stdio.h"
 #include <assert.h>
 
-FILE *fopen (const char *file,
-       const char *mode)
-{
-  // printf("I am here\n");
-  return 1;
-  // return _fopen_r (_REENT, file, mode);
-}
+// FILE *fopen (const char *file,
+//        const char *mode)
+// {
+//   // printf("I am here\n");
+//   return 1;
+//   // return _fopen_r (_REENT, file, mode);
+// }
 #define SYS_open 2
 int main() {
   printf("I am in file-test:%d\n",_syscall_(SYS_open, (int)&"/share/files/num", 0, 0));
@@ -15,34 +15,34 @@ int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   printf("I am in file-test:%d\n",fp);
 
-  // fseek(fp, 0, SEEK_END);
-  // long size = ftell(fp);
-  // assert(size == 5000);
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+  assert(size == 5000);
 
-  // fseek(fp, 500 * 5, SEEK_SET);
-  // int i, n;
-  // for (i = 500; i < 1000; i ++) {
-  //   fscanf(fp, "%d", &n);
-  //   assert(n == i + 1);
-  // }
+  fseek(fp, 500 * 5, SEEK_SET);
+  int i, n;
+  for (i = 500; i < 1000; i ++) {
+    fscanf(fp, "%d", &n);
+    assert(n == i + 1);
+  }
 
-  // fseek(fp, 0, SEEK_SET);
-  // for (i = 0; i < 500; i ++) {
-  //   fprintf(fp, "%4d\n", i + 1 + 1000);
-  // }
+  fseek(fp, 0, SEEK_SET);
+  for (i = 0; i < 500; i ++) {
+    fprintf(fp, "%4d\n", i + 1 + 1000);
+  }
 
-  // for (i = 500; i < 1000; i ++) {
-  //   fscanf(fp, "%d", &n);
-  //   assert(n == i + 1);
-  // }
+  for (i = 500; i < 1000; i ++) {
+    fscanf(fp, "%d", &n);
+    assert(n == i + 1);
+  }
 
-  // fseek(fp, 0, SEEK_SET);
-  // for (i = 0; i < 500; i ++) {
-  //   fscanf(fp, "%d", &n);
-  //   assert(n == i + 1 + 1000);
-  // }
+  fseek(fp, 0, SEEK_SET);
+  for (i = 0; i < 500; i ++) {
+    fscanf(fp, "%d", &n);
+    assert(n == i + 1 + 1000);
+  }
 
-  // fclose(fp);
+  fclose(fp);
 
   printf("PASS!!!\n");
 
