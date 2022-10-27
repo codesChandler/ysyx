d2b911b29feb7476f22e99c824f9deaa89d9741b
ysyx_22040632-Xiang Hui <2434967096@qq.com>
2022-10-27 18:33:33 +0800

    add exam file

--- ysyx-exam/exam-init.sh
+++ ysyx-exam/exam-init.sh
@@ -7,6 +7,13 @@ if [[ ! -d navy-apps/$d ]]; then
 else
   echo navy-apps/$d already exists
 fi
+d="apps/bird/repo"
+if [[ ! -d navy-apps/$d ]]; then
+  cp -r $NAVY_HOME/$d `dirname navy-apps/$d`
+  mkdir -p navy-apps/fsimg/share/games
+else
+  echo navy-apps/$d already exists
+fi
 d="libs/libc"
 if [[ ! -d navy-apps/$d ]]; then
   cp -r $NAVY_HOME/$d `dirname navy-apps/$d`
@@ -21,6 +28,13 @@ else
   echo nemu/$d already exists
 fi
 
+d="../am-kernels"
+if [[ ! -d nemu/$d ]]; then
+  cp -r $NEMU_HOME/$d `dirname nemu/$d`
+else
+  echo `dirname nemu/$d` already exists
+fi
+
 source ysyx-exam/setup-env.sh
 
 find $NAVY_HOME -name Makefile | xargs sed -i -e '/git clone/d'
@@ -31,3 +45,6 @@ make -C $NEMU_HOME -j distclean
 touch $NEMU_HOME/.config
 cp ysyx-exam/exam_defconfig $NEMU_HOME/configs/
 make -C $NEMU_HOME exam_defconfig
+d="ISA=native"
+make -C $NAVY_HOME/apps/pal $d install
+make -C $NAVY_HOME/apps/bird $d install
