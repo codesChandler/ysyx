043f69c216088ddb27a41000079a45fe2b22f109
ysyx_22040632-Xiang Hui <2434967096@qq.com>
2023-02-02 17:04:03 +0800

    before test

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,10 +1,11 @@
 #include <common.h>
 extern void do_syscall(Context *c);
+extern Context* schedule(Context *prev);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_SYSCALL:do_syscall(c);break;
-    case EVENT_YIELD:Log("EVENT_YIELD\n");break;
+    case EVENT_YIELD:c=schedule(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,23 +13,41 @@ void switch_boot_pcb() {
 }
 
 void hello_fun(void *arg) {
-  int j = 1;
+  int j=1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+    Log("Hello World from Nanos-lite with arg '%c' for the %dth time!", arg, j);
     j ++;
     yield();
   }
 }
 
+void context_kload(PCB *pcb, void (*entry) (void *), void *arg){
+  Area kstack;
+  kstack.start=(void *)(pcb->stack);
+  kstack.end=(void*)(pcb->stack+sizeof(pcb->stack));
+  kcontext(kstack,entry, arg);
+}
+
 void init_proc() {
+  context_kload(&pcb[0], hello_fun, (void *)'A');
+  context_kload(&pcb[1], hello_fun, (void *)'B');
   switch_boot_pcb();
 
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/pal");
+  // naive_uload(NULL, "/bin/pal");
 }
 
 Context* schedule(Context *prev) {
-  return NULL;
+  // save the context pointer
+  current->cp = prev;
+
+  // always select pcb[0] as the new process
+  current = (current == &pcb[0]) ? &pcb[1] : &pcb[0];
+
+  // then return the new context
+  return current->cp;
 }
+
+
